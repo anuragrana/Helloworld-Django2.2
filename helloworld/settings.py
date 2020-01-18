5ac5340be4055d3df9b1aeeb474bb7102bf5814a
rana <anuragrana31189@gmail.com>
2020-01-18 22:51:29 +0530

    flash messages in django template

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -119,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from django.contrib.messages import constants as messages
+    MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+except Exception as e:
+    pass
--- hw/views.py
+++ hw/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
     data = dict()
+    messages.success(request, "Success: This is the sample success Flash message.")
+    messages.error(request, "Error: This is the sample error Flash message.")
+    messages.info(request, "Info: This is the sample info Flash message.")
+    messages.warning(request, "Warning: This is the sample warning Flash message.")
     return render(request, 'hw/home.html', data)
