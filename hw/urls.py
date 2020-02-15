d6d6113486f649f1b57c2888fad0b843dd772fab
rana <anuragrana31189@gmail.com>
2020-02-15 17:06:24 +0530

    query params from request in Django

--- hw/urls.py
+++ hw/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "hw"
 urlpatterns = [
     path(r'', views.home, name='home'),
+    path(r'get-request-example/', views.get_request_example, name='get_request_example'),
 ]
--- hw/views.py
+++ hw/views.py
@@ -5,3 +5,13 @@ from django.shortcuts import render
 def home(request):
     data = dict()
     return render(request, 'hw/home.html', data)
+
+
+def get_request_example(request):
+    # print complete QueryDict
+    print(request.GET)
+    # print one value of param1
+    print(request.GET.get('param1'))
+    # print whole list of values for param1
+    # print(request.GET.getlist('param1'))
+    return render(request, 'hw/home.html', {})
