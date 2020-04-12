4e6e651a379c86d8e40f9fc31b3685dde00e7498
rana <anuragrana31189@gmail.com>
2020-04-12 17:15:48 +0530

    different post request body examples with postman collection

--- hw/urls.py
+++ hw/urls.py
@@ -5,4 +5,11 @@ from . import views
 app_name = "hw"
 urlpatterns = [
     path(r'', views.home, name='home'),
+    path(r'post-example/raw/', views.post_example_raw, name='post_example_raw'),
+    path(r'post-example/form-urlencoded/', views.post_example_form_urlencoded, name='post_example_form_urlencoded'),
+    path(r'post-example/form-data/', views.post_example_form_data, name='post_example_form_data'),
+    path(r'post-example/binary/', views.post_example_binary, name='post_example_binary'),
+    path(r'post-example/none/', views.post_example_none, name='post_example_none'),
+    path(r'post-example/file-upload/', views.post_example_file_upload, name='post_example_file_upload'),
+
 ]
--- hw/views.py
+++ hw/views.py
@@ -1,7 +1,57 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+from urllib.parse import parse_qs
 
 
 # Create your views here.
 def home(request):
     data = dict()
     return render(request, 'hw/home.html', data)
+
+
+@csrf_exempt
+def post_example_raw(request):
+    print('POST', request.POST)
+    print('BODY', request.body)
+    print('JSON', json.loads(request.body.decode('utf-8')))
+    print('FILES', request.FILES)
+    return render(request, 'hw/home.html', {})
+
+
+@csrf_exempt
+def post_example_form_urlencoded(request):
+    print('POST', request.POST)
+    print('BODY', request.body)
+    print(parse_qs(request.body.decode('utf-8')))
+    return render(request, 'hw/home.html', {})
+
+
+@csrf_exempt
+def post_example_form_data(request):
+    print('POST', request.POST)
+    print('FILES', request.FILES)
+    return render(request, 'hw/home.html', {})
+
+
+@csrf_exempt
+def post_example_binary(request):
+    print('POST', request.POST)
+    print('BODY', request.body)
+    print('FILES', request.FILES)
+    return render(request, 'hw/home.html', {})
+
+
+@csrf_exempt
+def post_example_none(request):
+    print('POST', request.POST)
+    print('BODY', request.body)
+    print('FILES', request.FILES)
+    return render(request, 'hw/home.html', {})
+
+
+@csrf_exempt
+def post_example_file_upload(request):
+    print('POST', request.POST)
+    print('FILES', request.FILES)
+    return render(request, 'hw/home.html', {})
